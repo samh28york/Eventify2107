8516270d3aaee91a89196e8d9f9a52c0afab750d
Komal <komaldha@my.yorku.ca>
2024-11-22 18:49:42 -0500

    added seed file and fixed merge errors

--- db/migrate/20241122183829_create_guest_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateGuestLists < ActiveRecord::Migration[7.2]
-  def change
-    create_table :guest_lists do |t|
-      t.references :event, null: false, foreign_key: true
-      t.references :guest, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_22_183829) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_20_204731) do
   create_table "events", force: :cascade do |t|
     t.datetime "start_date"
     t.datetime "end_date"
