d93d973a2f04cec274a783fdfc458023ad3f5301
Komal <komaldha@my.yorku.ca>
2024-11-22 14:33:32 -0500

    updated styling with rubocop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: [ :index ]
-  
+
   def index
     @events = Event.joins(:guests).where(guests: { user_id: current_user.id })
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,3 @@ class UsersController < ApplicationController
         params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
     end
 end
-
