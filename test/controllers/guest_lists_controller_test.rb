6a7a046ca368c1bb9b47f9f81db4b6043e802932
midhatdlg <midhat.azad@shopify.com>
2024-11-22 20:14:39 -0500

    Remove All guest_list features and attributes

--- test/controllers/guest_lists_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class GuestlistsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get guest_lists_index_url
-    assert_response :success
-  end
-end
