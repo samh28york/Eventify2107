0b93b73ee413e1d653a7a628f72fa5410bbfa54d
220918306 <parsahm12@gmail.com>
2024-11-25 23:45:53 -0500

    changes to add model tests, delete old organizer test, and delete all fixtures that were interrupting tests.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,7 +14,54 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      first_name: "John",
+      last_name: "Doe",
+      email: "johndoe@example.com",
+      password: "password123",
+      password_confirmation: "password123"
+    )
+  end
+
+  test "should be valid with all attributes" do
+    assert @user.valid?
+  end
+
+  test "should require a first name" do
+    @user.first_name = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:first_name], "can't be blank"
+  end
+
+  test "should require a last name" do
+    @user.last_name = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:last_name], "can't be blank"
+  end
+
+  test "should require an email" do
+    @user.email = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:email], "can't be blank"
+  end
+
+  test "should require a unique email" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+    assert_includes duplicate_user.errors[:email], "has already been taken"
+  end
+
+  test "should require a password" do
+    @user.password = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:password], "can't be blank"
+  end
+
+  test "password and password confirmation should match" do
+    @user.password_confirmation = "different"
+    assert_not @user.valid?
+    assert_includes @user.errors[:password_confirmation], "doesn't match Password"
+  end
 end
